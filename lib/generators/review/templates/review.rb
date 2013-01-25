0c9b9a013a35cbea150290c199b70df834cb9c5e
Eric Steen <rubycoder1@gmail.com>
2013-01-25 08:26:23 -0800

    Update lib/generators/review/templates/review.rb

--- lib/generators/review/templates/review.rb
+++ lib/generators/review/templates/review.rb
@@ -3,14 +3,13 @@ class Review < ActiveRecord::Base
   include ActsAsReviewable::Review
 
   belongs_to :reviewable, :polymorphic => true
+  belongs_to :reviewer, :polymorphic => true
 
   default_scope :order => 'created_at ASC'
 
   # NOTE: install the acts_as_votable plugin if you
-  # want user to vote on the quality of comments.
+  # want votes on reviews.
   #acts_as_voteable
 
-  # NOTE: Comments belong to a user
-  belongs_to :user
 end
 
